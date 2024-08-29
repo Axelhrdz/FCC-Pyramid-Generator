38be292c583cfd449cb9981effe8c69c81725a6a
Axelhrdz <axelhernandezperez23@gmail.com>
2024-08-28 20:03:18 -0600

    adding basic functions to create the pyramid

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 //importing variables
-const pyramidContainer = document.querySelector('.pyramidContainer');
-console.log(pyramidContainer);
+const container = document.querySelector('.container');
+const pyramid = document.querySelector('.pyramid');
+const row = document.querySelector('.row');
+
+console.log(container);
+console.log(pyramid);
+console.log(row);
+
 
 //Function #1
 const character = '#';
 const rows = [];
-const counter = 8;
+const counter = 20;
 
 function creatingRows(counter){
     for(let i = 0; i < counter; i++){
-        
         rows.push(i+1);
-        console.log(i);
     }
 }
 creatingRows(counter);
 console.log(rows);
 
+
+
 //login characters in console function
+function loginCharacters(array, char){
+    for(let i = 0; i < array.length; i++){
+        let rowContent =  document.createElement('li');
+        rowContent.classList.add('row');
+
+        rowContent.textContent = ' '.repeat(array.length - array[i]) + char.repeat(array[i] * 2 - 1);
+        pyramid.appendChild(rowContent);
+
+        console.log(rowContent);
+    }
+}
+
+loginCharacters(rows, character);
