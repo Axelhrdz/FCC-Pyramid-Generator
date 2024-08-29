e6e3f220da911f11f25a0b48b7fdbab2c0991537
Axelhrdz <axelhernandezperez23@gmail.com>
2024-08-28 22:22:49 -0600

    finshing functionality of the piramid

--- script.js
+++ script.js
@@ -2,38 +2,63 @@
 const container = document.querySelector('.container');
 const pyramid = document.querySelector('.pyramid');
 const row = document.querySelector('.row');
+const form = document.querySelector('.pyrForm');
+const pyrText = document.getElementById('pyrText');
+const pyrLength = document.getElementById('pyrLength');
+const submitButton = document.getElementById('pyrSubmit'); 
 
 console.log(container);
 console.log(pyramid);
 console.log(row);
+console.log(form);
+console.log(pyrText);
+console.log(pyrLength);
+console.log(submitButton);
 
 
 //Function #1
-const character = '#';
+let character;
 const rows = [];
-const counter = 20;
+let counter;
 
-function creatingRows(counter){
-    for(let i = 0; i < counter; i++){
-        rows.push(i+1);
+
+
+//Submiting form and getting values
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    character = pyrText.value.trim();
+    counter = pyrLength.value;
+
+    console.log(character);
+    console.log(counter);
+
+    function creatingRows(counter){
+        for(let i = 0; i < counter; i++){
+            rows.push(i+1);
+        }
     }
-}
-creatingRows(counter);
-console.log(rows);
-
-
-
-//login characters in console function
-function loginCharacters(array, char){
-    for(let i = 0; i < array.length; i++){
-        let rowContent =  document.createElement('li');
-        rowContent.classList.add('row');
-
-        rowContent.textContent = ' '.repeat(array.length - array[i]) + char.repeat(array[i] * 2 - 1);
-        pyramid.appendChild(rowContent);
-
-        console.log(rowContent);
+    creatingRows(counter);
+    console.log(rows);
+    
+    
+    
+    //login characters in console function
+    function loginCharacters(array, char){
+        for(let i = 0; i < array.length; i++){
+            let rowContent =  document.createElement('li');
+            rowContent.classList.add('row');
+    
+            rowContent.textContent = ' '.repeat(array.length - array[i]) + char.repeat(array[i] * 2 - 1);
+            pyramid.appendChild(rowContent);
+    
+            console.log(rowContent);
+        }
     }
-}
+    
+    loginCharacters(rows, character);
 
-loginCharacters(rows, character);
+})
+
+
+
